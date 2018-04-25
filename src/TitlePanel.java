f6f332456560a6f1e1b3eb0207720368e13f687b
Daylen Hawthorne <hawthord@student.psdr3.org>
2018-04-25 12:20:50 -0500

    Changed Title panel

--- src/TitlePanel.java
+++ src/TitlePanel.java
@@ -26,9 +26,9 @@ public class TitlePanel extends JPanel {
 		btnNewButton.setBounds(155, 152, 117, 29);
 		add(btnNewButton);
 		
-		JLabel lblMazeGame = new JLabel("Maze Game");
+		JLabel lblMazeGame = new JLabel("NBA Playoff Maze");
 		lblMazeGame.setFont(new Font("KufiStandardGK", Font.BOLD, 29));
-		lblMazeGame.setBounds(134, 85, 161, 35);
+		lblMazeGame.setBounds(98, 90, 314, 35);
 		add(lblMazeGame);
 		
 		JButton btnCredits = new JButton("Credits");
