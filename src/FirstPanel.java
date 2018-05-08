eff436efc299a469369b39b3c9f97c50688bf0e8
dunbarr4 <33463168+dunbarr4@users.noreply.github.com>
2018-05-08 11:49:12 -0500

    Movinf Red Box

--- src/FirstPanel.java
+++ src/FirstPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.Timer;
@@ -14,6 +15,7 @@ public class FirstPanel extends JPanel {
 	private int ulx = 200, uly = 400;
 	private int currentDirection = 1;
 	private final int SIDE_LENGTH = 40, UP = 0, RIGHT = 1, DOWN = 2, LEFT= 3;
+	private int amt = 5;
 	/**
 	 * Create the panel.
 	 */
@@ -21,20 +23,20 @@ public class FirstPanel extends JPanel {
 		this.setBackground(Color.WHITE);
 		
 		//Left
-		getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "moveLeft");
-		getActionMap().put("moveLeft", new MoveAction(LEFT));
+		getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "moveLeft");
+		getActionMap().put("moveLeft", new MoveAction(-amt, 0));
 		
 		//Right
-		getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "moveRight");
-		getActionMap().put("moveRight", new MoveAction(RIGHT));
+		getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "moveRight");
+		getActionMap().put("moveRight", new MoveAction(amt, 0));
 		
 		//Up
-		getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "moveUp");
-		getActionMap().put("moveUp", new MoveAction(UP));
+		getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "moveUp");
+		getActionMap().put("moveUp", new MoveAction(0, -amt));
 		
 		//Down
-		getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "moveDown");
-		getActionMap().put("moveDown", new MoveAction(DOWN));
+		getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "moveDown");
+		getActionMap().put("moveDown", new MoveAction(0, amt));
 		
 	}
 		
@@ -57,18 +59,19 @@ public class FirstPanel extends JPanel {
 	
 	private class MoveAction extends AbstractAction {
 		
-		private int direction;
+		private int dx, dy;
 		
-		public MoveAction(int direction){
-			this.direction = direction;
+		public MoveAction(int dx, int dy){
+			this.dx = dx;
+			this.dy = dy;
 		}
 		
 		@Override
 		
 		public void actionPerformed(ActionEvent e){
-		
-			
-			currentDirection = direction;
+			ulx += dx;
+			uly += dy;
+			repaint();
 			
 		}
 		
